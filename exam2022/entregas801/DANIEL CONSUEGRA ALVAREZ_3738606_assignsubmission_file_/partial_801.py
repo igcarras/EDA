f038429c84a47f777326aeea5d3b017bb4d41396
israelgc <igcarras@inf.uc3m.es>
2022-03-14 20:07:48 +0100

    laptop

---
+++ exam2022/entregas801/DANIEL CONSUEGRA ALVAREZ_3738606_assignsubmission_file_/partial_801.py
@@ -0,0 +1,146 @@
+"""Daniel Consuegra alvarez"""
+class SNode:
+    def __init__(self, e, next=None):
+        self.elem = e
+        self.next = next
+
+
+class MySList():
+
+    def __init__(self):
+        self._head = None
+        self._tail = None
+
+    def __str__(self):
+        """Returns a string with the elements of the list"""
+        ###This functions returns the same format used
+        ###by the Python lists, i.e, [], ['i'], ['a', 'b', 'c', 'd']
+        ###[1], [3, 4, 5]
+        nodeIt = self._head
+        result = '['
+        while nodeIt:
+            result += str(nodeIt.elem) + ", "
+            nodeIt = nodeIt.next
+
+        if len(result) > 1:
+            result = result[:-2]
+
+        result += ']'
+        return result
+
+
+    def append(self, e):
+        """Adds a new element, e, at the end of the list"""
+        # create the new node
+        newNode = SNode(e)
+        # the last node must point to the new node
+        # now, we must update the tail reference
+        if self._head == None:
+            self._head = newNode
+        else:
+            self._tail.next = newNode
+
+        self._tail = newNode
+
+    def isSorted(self):
+        "returns True if self is sorted"
+        if self._head == None:
+            return True
+        else:
+            node1 = self._head
+            node2 = node1.next
+            while node2:
+                if node1.elem > node2.elem:
+                    return False
+                node1 = node2
+                node2 = node2.next
+
+        return True
+
+    def subtraction(self, other):
+        "Subtraction of elements in two ordered lists."
+        if not self.isSorted():
+            return None
+        else:
+            current=self._head
+            current2=other._head
+            cont=0
+            cont2=0
+            while current:
+                current=current.next
+                cont+=1
+            while current2:
+                current2=current2.next
+                cont2+=1
+            NodeIt1 = self._head
+            NodeIt2 = other._head
+            newList=MySList()
+            if cont>=cont2:
+                long=cont
+            else:
+                long=cont2
+            while long>0:
+                if self._head==None:
+                    return other
+                elif other._head==None:
+                    return self
+                else:
+                    if NodeIt1 and not NodeIt2:
+                        result=NodeIt1.elem
+                    elif NodeIt2 and not NodeIt1:
+                        result=NodeIt2.elem
+                    else:
+                        result=NodeIt1.elem-NodeIt2.elem
+                    newList.append(abs(result))
+                    if NodeIt1.next:
+                        NodeIt1 = NodeIt1.next
+                    if NodeIt2.next:
+                        NodeIt2 = NodeIt2.next
+                    long-=1
+            return newList
+import random
+
+if __name__ == '__main__':
+    # Please, uncomment the code for test each function
+    l2 = MySList()
+
+    for i in range(10):
+        l2.append(random.randint(0, 20))
+    print(l2)
+
+    l3 = MySList()
+    for i in range(10):
+        l3.append(i)
+
+    print('l2:', str(l2))
+    print('l3:', str(l3))
+
+    print("List subtraction:", str(l2.subtraction(l3)))
+    print("List subtraction:", str(l3.subtraction(l2)))
+
+    data = []
+    for i in range(5):
+        x = random.randint(0, 10)
+        if x not in data:
+            data.append(x)
+
+    data.sort()
+    l2 = MySList()
+    for x in data:
+        l2.append(x)
+
+    data = []
+    for i in range(7):
+        x = random.randint(0, 10)
+        if x not in data:
+            data.append(x)
+
+    data.sort()
+    l3 = MySList()
+    for x in data:
+        l3.append(x)
+
+    print('l2:', str(l2))
+    print('l3:', str(l3))
+    print("List subtraction:", str(l2.subtraction(l3)))
+    print("List subtraction:", str(l3.subtraction(l2)))
---
+++ exam2022/entregas801/HERMANN/partial_801.py
@@ -0,0 +1,131 @@
+class SNode:
+    def __init__(self, e, next=None):
+        self.elem = e
+        self.next = next
+
+
+class MySList():
+
+    def __init__(self):
+        self._head = None
+        self._tail = None
+
+    def __str__(self):
+        """Returns a string with the elements of the list"""
+        ###This functions returns the same format used
+        ###by the Python lists, i.e, [], ['i'], ['a', 'b', 'c', 'd']
+        ###[1], [3, 4, 5]
+        nodeIt = self._head
+        result = '['
+        while nodeIt:
+            result += str(nodeIt.elem) + ", "
+            nodeIt = nodeIt.next
+
+        if len(result) > 1:
+            result = result[:-2]
+
+        result += ']'
+        return result
+
+
+    def append(self, e):
+        """Adds a new element, e, at the end of the list"""
+        # create the new node
+        newNode = SNode(e)
+        # the last node must point to the new node
+        # now, we must update the tail reference
+        if self._head == None:
+            self._head = newNode
+        else:
+            self._tail.next = newNode
+
+        self._tail = newNode
+
+    def isSorted(self):
+        "returns True if self is sorted"
+        if self._head == None:
+            return True
+        else:
+            node1 = self._head
+            node2 = node1.next
+            while node2:
+                if node1.elem > node2.elem:
+                    return False
+                node1 = node2
+                node2 = node2.next
+
+        return True
+
+    def subtraction(self, other):
+        "Subtraction of elements in two ordered lists."
+         
+        nuevo=MySList()
+        nue=other()
+        cabeza= self._head
+        contador=0
+        while cabeza: 
+            if cabeza==None or self._tail ==None:
+                return None
+            else:
+                while nuevo or nue:
+                    nuevo.append(self._head)
+                    nuevo.append(self._tail)     
+                    nue.append(self._head) 
+                    nue.append(self._tail) 
+                    while nuevo or nue:
+                        result= self._head.elem - self._head.elem 
+                        cabeza= cabeza.next
+
+                        contador +=1 
+               
+                    self._tail=self._tail.next
+                self._size=-1
+                return result
+                        
+
+        
+import random
+
+if __name__ == '__main__':
+    # Please, uncomment the code for test each function
+    l2 = MySList()
+
+    for i in range(10):
+        l2.append(random.randint(0, 20))
+    print(l2)
+    l3 = MySList()
+    for i in range(10):
+        l3.append(i)
+
+    print('l2:', str(l2))
+    print('l3:', str(l3))
+
+    print("List subtraction:", str(l2.subtraction(l3)))
+    print("List subtraction:", str(l3.subtraction(l2)))
+
+    data = []
+    for i in range(5):
+        x = random.randint(0, 10)
+        if x not in data:
+            data.append(x)
+
+    data.sort()
+    l2 = MySList()
+    for x in data:
+        l2.append(x)
+
+    data = []
+    for i in range(7):
+        x = random.randint(0, 10)
+        if x not in data:
+            data.append(x)
+
+    data.sort()
+    l3 = MySList()
+    for x in data:
+        l3.append(x)
+
+    print('l2:', str(l2))
+    print('l3:', str(l3))
+    print("List subtraction:", str(l2.subtraction(l3)))
+    print("List subtraction:", str(l3.subtraction(l2)))
