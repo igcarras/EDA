a1e8f5127dc591dfd61212cce688c7c45bbb0777
IsraelGc <igcarras@inf.uc3m.es>
2022-04-20 16:32:15 +0200

    first

---
+++ labcase2022/esqueleto/phase1.py
@@ -0,0 +1,25 @@
+from slist import SList
+import sys
+
+class SList2(SList):
+
+    def sumLastN(self, n):
+        ...
+    
+    #method for inserting a new node in the middle
+    def insertMiddle(self, elem):
+        
+        ...
+
+    
+    def insertList(self,inputList,start,end):
+        ...
+
+
+    def reverseK(self,k):
+        ...
+
+
+    def maximumPair(self):
+        ...
+ 
