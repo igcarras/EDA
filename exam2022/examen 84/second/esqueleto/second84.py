3c87abff6cb44c7bb3e536d7680eedf9c2b2a480
IsraelGc <igcarras@inf.uc3m.es>
2022-04-24 19:40:33 +0200

    first

--- exam2022/examen 84/second/esqueleto/second84.py
+++ exam2022/examen 84/second/esqueleto/second84.py
@@ -46,7 +46,7 @@ class MyBinarySearchTree:
             node.right = self._insert(node.right, elem)
         return node
 
-    # Removes all nodes having value outside the given range
+    # Removes all leaf nodes having value outside the given range
     # returns a sorted list in ascending order
     def removeOutsideRange(self, min: int, max: int) -> []:
         # update the root with the new subtree after remove elem
@@ -54,27 +54,10 @@ class MyBinarySearchTree:
         self._removeOutsideRange(self._root, min, max, removelist)
         return removelist
 
-    # Removes all nodes having value outside the given range
+    # Removes all leaf nodes having value outside the given range
     # returns a sorted list in ascending order
     def _removeOutsideRange(self, node: BinaryNode, min: int, max: int, removelist: []) -> None:
-        # Base Case
-        if node is None:
-            return
-
-        # check is node is not leaf
-        if node.left or node.right:
-            node.left = self._removeOutsideRange(node.left, min, max,  removelist)
-            node.right = self._removeOutsideRange(node.right, min, max,  removelist)
-        else:
-            if (node.left is None) and (node.right is None) and (node.elem < min or node.elem > max):
-                # node is a leave
-                # append node in list
-                # print("leaf node for removing:", node.elem)
-                removelist.append(node.elem)
-                return None
-
-        # if node is not leaf, return node and continue recursion
-        return node
+      ...
 
 
 if __name__ == "__main__":
