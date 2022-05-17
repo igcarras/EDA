1eb2d56ca2c16456eff7dd36c9465a09e732a1d7
israelgc <igcarras@inf.uc3m.es>
2022-05-17 17:35:58 +0200

    laptop2

---
+++ exam2022/Final June 2022/Exer 2/exer2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class BinaryNode:
+
+    def __init__(self, elem: object, node_left: 'BinaryNode' = None, node_right: 'BinaryNode' = None) -> None:
+        self.elem = elem
+        self.left = node_left
+        self.right = node_right
+
+    def __str__(self):
+        return str(self.elem)
+
+class BinaryTree:
+    def __init__(self) -> None:
+        """creates an empty binary tree"""
+        self._root = None
+    
+    # Exam starts here...
+    
+    def right_sum(self) -> int:
+        ...
+
