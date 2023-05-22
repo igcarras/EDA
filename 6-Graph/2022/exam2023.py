b5c17a03c7bfa1cb304a83417d5c1b1e949608ee
israelgc <igcarras@inf.uc3m.es>
2023-05-22 18:01:27 +0200

    laptop2

--- 6-Graph/2022/exam2023.py
+++ 6-Graph/2022/exam2023.py
@@ -37,27 +37,24 @@ class MyGraph:
 
     def has_path_to_odd2(self, k) -> []:
         list_odd = []
-        visited = {}
         #recorre vertices del grafo
         for v in self._vertices.keys():
             #para cada vertice del grafo inicializa vertices visitados
-            for m in self._vertices.keys():
-                visited[m] = False
+
             #recorre el grafo en profundidad para cada vertice (con vertices visitados a false)
-            self._has_path_to_odd2(v, k, visited, v, list_odd)
+            self._has_path_to_odd2(v, k, v, list_odd)
 
         return list_odd
 
-    def _has_path_to_odd2(self, vertex, k, visited, origin, lista_parcial):
-        visited[vertex] = True
+    def _has_path_to_odd2(self, vertex, k, origin, lista_parcial):
         for u in self._vertices[vertex]:
-            if not visited[u]:
-                self._has_path_to_odd2(u, k - 1, visited, origin, lista_parcial)
-                if k == 0:
-                    return
-                elif vertex % 2 != 0 and vertex != origin:
+            if k == 0:
+                return
+            elif vertex % 2 != 0 and vertex != origin:
+                if origin not in lista_parcial:
                     lista_parcial.append(origin)
 
+            self._has_path_to_odd2(u, k - 1, origin, lista_parcial)
         return
 
 
@@ -79,26 +76,29 @@ if __name__ == '__main__':
     g.add_edge(3, 4)
     g.add_edge(2, 2)
 
-
+    print("g -----------")
     print(g.has_path_to_odd2(2))
+    print(g.has_path_to_odd2(3))
     print(g.has_path_to_odd(2))
-    # print(g.has_path_to_odd(3))
-    #
-    # l=[2,4,6,8,10,12,14,16,15]
-    # g1=MyGraph(l)
-    #
-    # g1.add_edge(4,6)
-    # g1.add_edge(4,8)
-    # g1.add_edge(6,12)
-    # g1.add_edge(8,10)
-    # g1.add_edge(12,16)
-    # g1.add_edge(10,14)
-    # g1.add_edge(14,15)
-    # g1.add_edge(12,15)
-    #
-    #
-    # print(g1.has_path_to_odd(13))
-    #
+    print(g.has_path_to_odd(3))
+    print("g -----------")
+    l=[2,4,6,8,10,12,14,16,15]
+    g1=MyGraph(l)
+
+    g1.add_edge(4,6)
+    g1.add_edge(4,8)
+    g1.add_edge(6,12)
+    g1.add_edge(8,10)
+    g1.add_edge(12,16)
+    g1.add_edge(10,14)
+    g1.add_edge(14,15)
+    g1.add_edge(12,15)
+
+    print("g1 starts  -----------")
+    print(g1.has_path_to_odd2(13))
+    print(g1.has_path_to_odd(13))
+    print("g1 end -----------")
+
     l=[2,4,6,8,10,12,14,16,17, 20, 22, 24, 25]
     g2=MyGraph(l)
 
@@ -122,6 +122,7 @@ if __name__ == '__main__':
     g2.add_edge(22,24)
     g2.add_edge(24,17)
     g2.add_edge(25,24)
+    print("g2 -----------")
     print(g2.has_path_to_odd2(2))
     print(g2.has_path_to_odd2(3))
     print(g2.has_path_to_odd2(13))
@@ -129,6 +130,7 @@ if __name__ == '__main__':
     print(g2.has_path_to_odd(2))
     print(g2.has_path_to_odd(3))
     print(g2.has_path_to_odd(13))
+    print("g2 -----------")
    #
     # l = []
     # g3 = MyGraph(l)
@@ -140,20 +142,25 @@ if __name__ == '__main__':
     # print(g4.has_path_to_odd(0))
     # print(g4.has_path_to_odd(-1))
     #
-    # l = [2,4,6,8,10,12,14,16,18,20]
-    # g5 = MyGraph(l)
-    # g5.add_edge(2, 4)
-    # g5.add_edge(4, 6)
-    # g5.add_edge(2, 10)
-    # g5.add_edge(8, 10)
-    # g5.add_edge(8, 12)
-    # g5.add_edge(8, 6)
-    # g5.add_edge(6, 12)
-    # g5.add_edge(6, 14)
-    # g5.add_edge(10, 16)
-    # g5.add_edge(16, 12)
-    # g5.add_edge(16, 20)
-    # g5.add_edge(20, 12)
-    # print(g5.has_path_to_odd(0))
-    # print(g5.has_path_to_odd(12))
+    l = [2,4,6,8,10,12,14,16,18,20]
+    g5 = MyGraph(l)
+    g5.add_edge(2, 4)
+    g5.add_edge(4, 6)
+    g5.add_edge(2, 10)
+    g5.add_edge(8, 10)
+    g5.add_edge(8, 12)
+    g5.add_edge(8, 6)
+    g5.add_edge(6, 12)
+    g5.add_edge(6, 14)
+    g5.add_edge(10, 16)
+    g5.add_edge(16, 12)
+    g5.add_edge(16, 20)
+    g5.add_edge(20, 12)
+
+    print("g5 -----------")
+    print(g5.has_path_to_odd(0))
+    print(g5.has_path_to_odd(12))
+    print(g5.has_path_to_odd2(0))
+    print(g5.has_path_to_odd2(12))
+    print("g5 -----------")
 
