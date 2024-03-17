5913901d9678b99e2a4527698f322ae688f8da05
israelgc <igcarras@inf.uc3m.es>
2024-03-17 19:37:17 +0100

    casa

--- examenes/23-24 Primer parcial/Final/Ejercicio2.py
+++ examenes/23-24 Primer parcial/Final/Ejercicio2.py
@@ -8,12 +8,11 @@ class SList2(SList):
         current = self._head
         last_node = self._tail
 
-        while current is not last_node.next:
+        while current:
             if current.next and (current.elem == current.next.elem):
                 print("estoy en ", current.elem)
 
                 duplicate = current.next
-                print("siguiente es:", duplicate.elem)
                 successive = duplicate.next
                 if successive is not None:
                     current.next = successive
@@ -21,13 +20,26 @@ class SList2(SList):
                     self._tail = duplicate
                     self._tail.next = None
                 else:
-                    print("entro aqui")
                     current = current.next
 
             else:
                 current = current.next
 
-
+    def move_duplicates_to_end_2(self):
+        if len(self) > 1:
+            prev = self._head
+            current = prev.next
+            final = self._tail
+            while current and prev != final:
+                if prev.elem == current.elem and current.next!=None:
+                    prev.next = current.next
+                    self._tail.next = current
+                    self._tail = current
+                    self._tail.next = None
+                    current = prev.next
+                else:
+                    prev = current
+                    current = current.next
 if __name__ == '__main__':
     slist = SList2()
     slist.add_last(1)
