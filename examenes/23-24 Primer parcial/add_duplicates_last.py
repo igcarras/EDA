85854f8a0a3cd6666335c165f4b7ce26420fdda9
israelgc <igcarras@inf.uc3m.es>
2025-03-11 15:17:02 +0100

    Marzo 2024

--- examenes/23-24 Primer parcial/add_duplicates_last.py
+++ examenes/23-24 Primer parcial/add_duplicates_last.py
@@ -19,16 +19,35 @@ class DList2(DList):
 
             return count
 
+    def removeSmaller(self, x):
+        current = self._head
+        while current:
+            next_node = current.next  # Guardar la referencia al siguiente nodo
+            if current.elem < x:
+                if current.prev:
+                    current.prev.next = current.next
+                else:
+                    self._head = current.next  # Actualizar cabeza si se elimina el primero
+
+                if current.next:
+                    current.next.prev = current.prev
+                else:
+                    self._tail = current.prev  # Actualizar cola si se elimina el último
+            current = next_node  # Avanzar al siguiente nodo
 
 if __name__ == '__main__':
     l = DList2()
-    l.addLast(4)
-    l.addLast(1)
-    l.addLast(3)
-    l.addLast(1)
-    l.addLast(2)
+    l.addLast(41)
+    l.addLast(12)
+    l.addLast(30)
+    l.addLast(6)
+    l.addLast(21)
     l.addLast(1)
 
     print(l)
-    print("Number duplicates:", l.add_duplicates_last(3))
+    #print("Number duplicates:", l.add_duplicates_last(3))
     print(l)
+
+    print("Lista original:", l)
+    l.removeSmaller(3)
+    print("Lista después de removeSmaller(3):", l)
